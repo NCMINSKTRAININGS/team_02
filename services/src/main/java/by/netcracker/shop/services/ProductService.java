229eec715135594c571c75dc38dee7bb601516fa
Nattiliana <nattiliana@gmail.com>
2016-12-10 16:31:24 +0300

    Product Dao Service

---
+++ services/src/main/java/by/netcracker/shop/services/ProductService.java
@@ -0,0 +1,16 @@
+package by.netcracker.shop.services;
+
+import by.netcracker.shop.pojo.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    Product finById(int id);
+
+    List<Product> findAll();
+
+    void save(Product product);
+
+    void deleteById(int id);
+}
