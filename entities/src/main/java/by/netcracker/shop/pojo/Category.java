329b16e007788b61ac06517049992fe00194747b
KOHEGYCb <dimaushenko@mail.ru>
2016-12-13 03:05:45 +0300

    Create Entities

---
+++ entities/src/main/java/by/netcracker/shop/pojo/Category.java
@@ -0,0 +1,114 @@
+package by.netcracker.shop.pojo;
+
+import java.util.Objects;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Dmitry
+ */
+@Entity
+@Table(name = "category")
+public class Category extends AbstractEntity {
+
+    @Column(name = "name", nullable = false, length = 45)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    public Category() {
+        super();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + Objects.hashCode(this.name);
+        hash = 83 * hash + Objects.hashCode(this.description);
+        hash = 83 * hash + Objects.hashCode(this.image);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return Objects.equals(this.image, other.image);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "name=" + name + ", description=" + description + ", image=" + image + '}';
+    }
+
+}
---
+++ entities/src/main/java/by/netcracker/shop/pojo/Manufacturer.java
@@ -0,0 +1,104 @@
+package by.netcracker.shop.pojo;
+
+import java.util.Objects;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Dmitry
+ */
+@Entity
+@Table(name = "manufacturer")
+public class Manufacturer extends AbstractEntity {
+
+    @Column(name = "name", nullable = false, length = 45)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "logo")
+    private String logo;
+
+    public Manufacturer() {
+        super();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the logo
+     */
+    public String getLogo() {
+        return logo;
+    }
+
+    /**
+     * @param logo the logo to set
+     */
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + Objects.hashCode(this.name);
+        hash = 19 * hash + Objects.hashCode(this.description);
+        hash = 19 * hash + Objects.hashCode(this.logo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Manufacturer other = (Manufacturer) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return Objects.equals(this.logo, other.logo);
+    }
+
+    @Override
+    public String toString() {
+        return "Manufacturer{" + "name=" + name + ", description=" + description + ", logo=" + logo + '}';
+    }
+
+}
