07802b0d0b7490944b4fb2f15941c29082935b2f
KOHEGYCb <dimaushenko@mail.ru>
2016-12-28 22:54:11 +0300

    a

--- services/src/main/java/by/netcracker/shop/services/impl/DeliveryServiceImpl.java
+++ services/src/main/java/by/netcracker/shop/services/impl/DeliveryServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service("deliveryService")
@@ -29,20 +30,22 @@ public class DeliveryServiceImpl implements DeliveryService {
     private static Logger logger = Logger.getLogger(DeliveryServiceImpl.class);
 
     @Override
-    public void insert(DeliveryDTO deliveryDTO) throws ServiceException {
+    public Long insert(DeliveryDTO deliveryDTO) throws ServiceException {
         Delivery deliveryPOJO = null;
+        Long id;
         try {
             if (deliveryDTO.getId() != null) {
                 deliveryPOJO = deliveryDAO.getById(deliveryDTO.getId());
             }
             if (deliveryPOJO == null)
                 deliveryPOJO = new Delivery();
-            deliveryDAO.insert(converter.toDeliveryPOJO(deliveryDTO, deliveryPOJO));
+            id = deliveryDAO.insert(converter.toDeliveryPOJO(deliveryDTO, deliveryPOJO));
         } catch (DAOException e) {
             logger.error(ServiceConstants.ERROR_SERVICE, e);
             TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
             throw new ServiceException(e);
         }
+        return id;
     }
 
     @Override
@@ -56,6 +59,23 @@ public class DeliveryServiceImpl implements DeliveryService {
             throw new ServiceException(e);
         }
         return converter.toDeliveryDTO(deliveryPOJO);
+    }
+
+    @Override
+    public void update(DeliveryDTO categoryDTO) throws ServiceException {
+        Delivery categoryPOJO = null;
+        try {
+            if (categoryDTO.getId() != null)
+                categoryPOJO = deliveryDAO.getById(categoryDTO.getId());
+            if (categoryPOJO == null) {
+                throw new ServiceException();
+            }
+            categoryPOJO = converter.toDeliveryPOJO(categoryDTO, categoryPOJO);
+            deliveryDAO.update(categoryPOJO);
+        } catch (DAOException e) {
+            logger.error(ServiceConstants.ERROR_SERVICE, e);
+            throw new ServiceException(e);
+        }
     }
 
     @Override
@@ -85,5 +105,33 @@ public class DeliveryServiceImpl implements DeliveryService {
             deliveryDTOs.add(converter.toDeliveryDTO(delivery));
         }
         return deliveryDTOs;
+    }
+
+    @Override
+    public Long getCount() throws ServiceException {
+        Long count;
+        try {
+            count = deliveryDAO.getCount();
+        } catch (DAOException e) {
+            logger.error(ServiceConstants.ERROR_SERVICE, e);
+            throw new ServiceException(e);
+        }
+        return count;
+    }
+
+    @Override
+    public List<DeliveryDTO> getByGap(int offset, int quantity) throws ServiceException {
+        List<Delivery> categoryPOJOs;
+        List<DeliveryDTO> categoryDTOs = new LinkedList<>();
+        try {
+            categoryPOJOs = deliveryDAO.getByGap(offset, quantity);
+        } catch (DAOException e) {
+            logger.error(ServiceConstants.ERROR_SERVICE, e);
+            throw new ServiceException(e);
+        }
+        for (Delivery categoryPOJO : categoryPOJOs) {
+            categoryDTOs.add(converter.toDeliveryDTO(categoryPOJO));
+        }
+        return categoryDTOs;
     }
 }
